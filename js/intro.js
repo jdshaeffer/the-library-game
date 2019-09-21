1b6067e007706f043e3911b7b486d1f44e281e24
jdshaeffer <shaefferjd@gmail.com>
2019-09-20 21:11:41 -0700

    map basic structure

--- js/intro.js
+++ js/intro.js
@@ -99,5 +99,165 @@ input.addEventListener("keyup", (event) => {
                 input.value = ""
             }
         }
+
+        else if(user.room === more_displays) {
+            if(x === "n") {
+                enter(displays, input, user, response)
+            }
+            else if(globals.includes(x) || globals.includes(x.slice(0,5))) {
+                printGlobals(x, displays, response, input, user)
+                input.value = ""
+            }
+            else if(user.room.walls.includes(x)) {
+                response.innerHTML = "can't go that way"
+                input.value = ""
+            }
+            else {
+                response.innerHTML = "what?"
+                input.value = ""
+            }
+        }
+
+        else if(user.room === door_to_viola_room) {
+            if(x === "n") {
+                enter(n_s_hallway, input, user, response)
+            }
+            else if(x === "w") {
+                enter(displays, input, user, response)
+            }
+            else if(globals.includes(x) || globals.includes(x.slice(0,5))) {
+                printGlobals(x, displays, response, input, user)
+                input.value = ""
+            }
+            else if(user.room.walls.includes(x)) {
+                response.innerHTML = "can't go that way"
+                input.value = ""
+            }
+            else {
+                response.innerHTML = "what?"
+                input.value = ""
+            }
+        }
+
+        else if(user.room === door_to_room4425) {
+            if(x === "s") {
+                enter(n_s_hallway, input, user, response)
+            }
+            else if(x === "n") {
+                enter(door_to_room4424, input, user, response)
+            }
+            else if(x === "w") {
+                response.innerHTML = "it's locked"
+                input.value = ""
+            }
+            else if(globals.includes(x) || globals.includes(x.slice(0,5))) {
+                printGlobals(x, displays, response, input, user)
+                input.value = ""
+            }
+            else if(user.room.walls.includes(x)) {
+                response.innerHTML = "can't go that way"
+                input.value = ""
+            }
+            else {
+                response.innerHTML = "what?"
+                input.value = ""
+            }
+        }
+
+        else if(user.room === door_to_room4424) {
+            if(x === "s") {
+                enter(door_to_room4425, input, user, response)
+            }
+            else if(x === "n") {
+                enter(intersection1, input, user, response)
+            }
+            else if(x === "w") {
+                response.innerHTML = "it's locked"
+                input.value = ""
+            }
+            else if(globals.includes(x) || globals.includes(x.slice(0,5))) {
+                printGlobals(x, displays, response, input, user)
+                input.value = ""
+            }
+            else if(user.room.walls.includes(x)) {
+                response.innerHTML = "can't go that way"
+                input.value = ""
+            }
+            else {
+                response.innerHTML = "what?"
+                input.value = ""
+            }
+        }
+
+        else if(user.room === intersection) {
+            if(x === "w") {
+                enter(e_w_hallway, input, user, response)
+            }
+            else if(x === "s") {
+                enter(door_to_room4424, input, user, response)
+            }
+            else if(x === "n") {
+                enter(e_w_hallway, input, user, response)
+            }
+            else if(globals.includes(x) || globals.includes(x.slice(0,5))) {
+                printGlobals(x, displays, response, input, user)
+                input.value = ""
+            }
+            else if(user.room.walls.includes(x)) {
+                response.innerHTML = "can't go that way"
+                input.value = ""
+            }
+            else {
+                response.innerHTML = "what?"
+                input.value = ""
+            }
+        }
+
+        else if(user.room === e_w_hallway) {
+            if(x === "w") {
+                enter(door_to_harps, input, user, response)
+            }
+            else if(x === "e") {
+                enter(intersection, input, user, response)
+            }
+            else if(globals.includes(x) || globals.includes(x.slice(0,5))) {
+                printGlobals(x, displays, response, input, user)
+                input.value = ""
+            }
+            else if(user.room.walls.includes(x)) {
+                response.innerHTML = "can't go that way"
+                input.value = ""
+            }
+            else {
+                response.innerHTML = "what?"
+                input.value = ""
+            }
+        }
+
+        else if(user.room === door_to_harps) {
+            if(x === "e") {
+                enter(e_w_hallway, input, user, response)
+            }
+            else if(x === "s") {
+                enter(harp_archives, input, user, response)
+            }
+            else if(globals.includes(x) || globals.includes(x.slice(0,5))) {
+                printGlobals(x, displays, response, input, user)
+                input.value = ""
+            }
+            else if(user.room.walls.includes(x)) {
+                response.innerHTML = "can't go that way"
+                input.value = ""
+            }
+            else {
+                response.innerHTML = "what?"
+                input.value = ""
+            }
+        }
+
+        else if(user.room === harp_archives) {
+            response.innerHTML = "you made it"
+            input.value = ""
+        }
     }
 })
